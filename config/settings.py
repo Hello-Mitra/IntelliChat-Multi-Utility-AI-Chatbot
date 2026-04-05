3d594b8ed06c63b92cef05b378006e1a5545aa24
Mitra <arijit.mitra22@gmail.com>
2026-04-06 02:11:39 +0530

    docker-compose, cicd added

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
-        extra = "ignore"  # ✅ this is the fix
+        extra = "ignore"
 
 
 settings = Settings()
